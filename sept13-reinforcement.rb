b17c79879554d6e03460af5ab47d9f7a8ec7d646
ilyana19 <aries_2152@hotmail.com>
2018-09-12 23:05:48 -0400

    sept13, trying out each_cons method

--- sept13-reinforcement.rb
+++ sept13-reinforcement.rb
@@ -11,13 +11,31 @@ class Trip
   end
 
   def add_location(location)
-    @destinations << location
+    @destinations << location.name
+  end
+
+  def travel_log
+    puts "Began Trip."
+    @destinations.each_cons(3) do |destination|
+      p destination
+    end
+    puts "Ended Trip."
   end
 end
 
-p toronto = Location.new("Toronto")
+toronto = Location.new("Toronto")
+ottawa = Location.new("Ottawa")
+montreal = Location.new("Montreal")
+quebec_city = Location.new("Quebec City")
+halifax = Location.new("Halifax")
+st_johns = Location.new("St. John's")
 
 trip = Trip.new
-
 trip.add_location(toronto)
-p trip
+trip.add_location(ottawa)
+trip.add_location(montreal)
+trip.add_location(quebec_city)
+trip.add_location(halifax)
+trip.add_location(st_johns)
+
+p trip.travel_log
